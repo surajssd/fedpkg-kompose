baab3863ba25c00c609b1ca309e1ee69ebd3973f
Suraj Deshmukh <surajssd009005@gmail.com>
2017-03-09 10:41:02 +0530

    Moved from godeps to glide so new way to parse dependency
    kompose moved from godeps to glide so now we there is no
    godeps.json but it's glide.lock file which needs to be parsed

--- parsedeps.go
+++ parsedeps.go
@@ -1,45 +1,47 @@
-// Helper for parsing Godeps.json file and spitting out
+// Helper for parsing glide.lock file and spitting out
 // bundled provides statements for an rpm spec file.
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"path"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
-type Dep struct {
-	ImportPath string
-	Comment    string
-	Rev        string
+type Import struct {
+	Name        string
+	Version     string
+	Subpackages []string
 }
 
-type GoDeps struct {
-	ImportPath   string
-	GoVersion    string
-	GodepVersion string
-	Packages     []string
-	Deps         []*Dep
+type Glide struct {
+	Hash        string
+	Updated     string
+	Imports     []Import
+	TestImports []Import
 }
 
 func main() {
-	var deps GoDeps
-	var x []byte
-	var err error
-
-	deps = GoDeps{}
-	if x, err = ioutil.ReadFile("Godeps.json"); err != nil {
-		fmt.Println(err)
-		return
+	yamlFile, err := ioutil.ReadFile("glide.lock")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Read in the json data and parse into Go structures
-	json.Unmarshal(x, &deps)
+	var glide Glide
+	err = yaml.Unmarshal(yamlFile, &glide)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Iterate over the deps that were unmarshaled and print
-	// out bundle information for each one.
-	for _, v := range deps.Deps {
-		fmt.Printf("Provides: bundled(golang(%s)) = %s-%s\n",
-			v.ImportPath, "%{version}", v.Rev)
+	for _, imp := range glide.Imports {
+		// we need format like this:
+		// Provides: bundled(golang(github.com/coreos/go-oidc/oauth2)) = %{version}-5cf2aa52da8c574d3aa4458f471ad6ae2240fe6b
+		for _, subp := range imp.Subpackages {
+			name := path.Join(imp.Name, subp)
+			fmt.Printf("Provides: bundled(golang(%s)) = %s-%s\n", name, "%{version}", imp.Version)
+		}
 	}
 }
